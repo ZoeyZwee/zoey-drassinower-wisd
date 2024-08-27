374b3c1778d9a071e97fcd11f6375a0233a2fcab
ZoeyZwee <thisemailistaken23@gmail.com>
2024-08-27 18:54:39 -0400

    Reorganized Interactive Plot settings

--- pages/1_Data Explorer.py
+++ pages/1_Data Explorer.py
@@ -1,8 +1,6 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
-import pickle
 
 df = pd.read_csv("data/WISD events.csv")
 df = df[df.parse_error.isna()]
@@ -12,107 +10,123 @@ df = df[df.has_bat & df.has_hit]
 st.title("Data Explorer")
 st.write("Use this interactive plot to compare features and discover trends in the data!")
 
-# region Batter Dropdown
+st.subheader("Configure Plot")
+# region Filters
+with st.expander("Filters"):
+    result_cols = st.columns(2)
+    with result_cols[0]:
+        include_hits = st.checkbox("Include Hits", value=True)
+    with result_cols[1]:
+        include_fouls = st.checkbox("Include Fouls", value=True)
 
-st.subheader("Select Batters")
-batter_counts = df.batter_name.value_counts()
-batters = st.multiselect(
-    "Batter (# of hits)",
-    batter_counts.index,
-    format_func=lambda name: f"{name} ({batter_counts[name]})",
-    placeholder="select batters..."
-)
-if batters:
-    df = df[df.batter_name.isin(batters)]
-# endregion
+    hit_mask = (df.result == "HitIntoPlay") == include_hits
+    foul_mask = (df.result == "Strike") == include_fouls
+    df = df[hit_mask | foul_mask]
 
-# region Stats Table
-with st.expander("Highlight Data"):
-    st.write("Use the left-most column to highlight points in the interactive plot!")
-    st_df = st.dataframe(
-        df,
-        on_select="rerun",
-        selection_mode="multi-row",
-        column_order=["batter_name", "result", "action", "xxBA", "exit_velocity", "head_speed",
-                      "launch_angle", "spray_angle", "bat_elevation", "bat_forward_tilt"],
-        hide_index=True
+    batter_filter_options = {
+        "all": "Include all batters",
+        "include": "Only include selected batters",
+        "exclude": "Exclude selected batters"
+    }
+    batter_filter = st.selectbox("Filter Batters",
+                                 options=batter_filter_options.keys(),
+                                 format_func=batter_filter_options.get,
+                                 )
+    if batter_filter in ["include", "exclude"]:
+        batter_counts = df.batter_name.value_counts()
+        batters = st.multiselect(
+            "Batter (# of hits)",
+            batter_counts.index,
+            format_func=lambda name: f"{name} ({batter_counts[name]})",
+            placeholder="select batters..."
+        )
+        if batter_filter == "include":
+            df = df[df.batter_name.isin(batters)]
+        elif batter_filter == "exclude":
+            df = df[~df.batter_name.isin(batters)]
+# endregion Filters
+# region Colour Groups
+with st.expander("Colour Groups"):
+    group_feature = st.selectbox(
+        label="Group By",
+        options=["None", "batter_name", "result"],
+        index=1
+    )
+    match group_feature:
+        case "None":
+            groups = [("_", True)]  # nightmare fuel line??
+        case _:
+            group_options = df[group_feature].value_counts()
+            group_highlight = st.multiselect(
+                label="Highlight Selection",
+                options=group_options.keys(),
+                format_func=lambda name: f"{name} ({group_options[name]})",
+                default=group_options if len(group_options) < 5 else None,
+                placeholder="Select multiple...",
+            )
+
+            groups = [(name, df[group_feature] == name) for name in group_highlight]
+            groups.insert(0, ("_all", True))
+    st.write("Note: Changing filter settings may cause Colour Groups to reset")
+# endregion Colour Groups
+
+# region Features
+feature_cols = st.columns(2)
+with feature_cols[0]:
+    x_feature = st.selectbox(
+        label="X",
+        options=["xxBA", "exit_velocity", "head_speed", "launch_angle",
+                 "spray_angle", "bat_elevation", "bat_forward_tilt"],
+        index=3
     )
 
-# endregion
-
-# region Select Plotting Features
-st.subheader("Select Features")
-c1, c2, c3 = st.columns(3)
-with c1:
-    x_feature = st.selectbox("X",
-                             ["xxBA", "exit_velocity", "head_speed", "launch_angle", "spray_angle", "bat_elevation",
-                              "bat_forward_tilt"],
-                             index=3
-                             )
-with c2:
-    y_feature = st.selectbox("Y",
-                             ["xxBA", "exit_velocity", "head_speed", "launch_angle", "spray_angle", "bat_elevation",
-                              "bat_forward_tilt"],
-                             index=1
-                             )
-with c3:
-    group_feature = st.selectbox("Group By",
-                                 ["None", "batter_name", "result"])
-    if group_feature == "None":
-        group_feature = None  # nightmare fuel line??
-
-
-with st.expander(label="Set Data Range"):
+    with feature_cols[1]:
+        y_feature = st.selectbox(
+            label="Y",
+            options=["xxBA", "exit_velocity", "head_speed", "launch_angle",
+                     "spray_angle", "bat_elevation", "bat_forward_tilt"],
+            index=1
+        )
+# endregion Features
+# region Data Range
+with st.expander("Data Range"):
     x_min, x_max = st.slider(
-        label=x_feature,
+        label="X Range",
         min_value=df[x_feature].min(),
         max_value=df[x_feature].max(),
-        step=0.01,
+        step=0.5,
         value=(df[x_feature].min(), df[x_feature].max()),
         format="%.1f",
         key="x slider"
     )
     y_min, y_max = st.slider(
-        label=y_feature,
+        label="Y Range",
         min_value=df[y_feature].min(),
         max_value=df[y_feature].max(),
-        step=0.01,
+        step=0.5,
         value=(df[y_feature].min(), df[y_feature].max()),
         format="%.1f",
         key="y slider"
     )
 
-# apply data range
-df = df[(df[x_feature]>x_min) & (df[x_feature]<x_max) & (df[y_feature]>y_min) & (df[y_feature]<y_max)]
-# create groups
-if group_feature is None:
-    groups = [("", slice(None))]  # includes everything
-else:
-    # a group is a boolean vector for indexing into df
-    # groups is a list of groups
-    groups = [(g, df[group_feature] == g) for g in df[group_feature].unique()]
+# True if point is within bounds
+range_mask = ((df[x_feature] > x_min) &
+              (df[x_feature] < x_max) &
+              (df[y_feature] > y_min) &
+              (df[y_feature] < y_max))
 
-# endregion
-
-# region Show Plot
+# endregion Data Range
+# region Generate Figure
 fig, ax = plt.subplots()
 ax.set_xlabel(x_feature)
 ax.set_ylabel(y_feature)
-for (label, g) in groups:
+for label, g in groups:
     ax.scatter(
-        (df[g])[[x_feature]],
-        (df[g])[[y_feature]],
+        (df[g & range_mask])[[x_feature]],
+        (df[g & range_mask])[[y_feature]],
         label=label
     )
-# highlight selected swings
-if st_df.selection.rows:
-    # get row data for highlighted swing
-    highlights = df.iloc[st_df.selection.rows]
-    ax.scatter(highlights[[x_feature]], highlights[[y_feature]],
-               marker="o", facecolors="none", edgecolors="magenta",
-               linewidth=1, s=85)
 
-if group_feature is not None:
-    ax.legend()
+ax.legend()
 st.pyplot(fig=fig)
 # endregion
