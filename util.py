c2ec68bd45faa83fb29e0930875f35ff46a3b704
ZoeyZwee <thisemailistaken23@gmail.com>
2024-09-17 13:23:15 -0400

    Updated file parsing

--- util.py
+++ util.py
@@ -3,9 +3,10 @@ For data structures and useful functions
 """
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.polynomial import Polynomial
 
 
 @dataclass
@@ -14,35 +15,35 @@ class Frame:
     x: float
     y: float
     z: float
+    speed: Optional[float] = None
+    vx: Optional[float] = None
+    vy: Optional[float] = None
+    vz: Optional[float] = None
 
-
-def visualize_pitch(fig, frames, bat_buffer=0.07, ball_buffer=0.3):
-    """ Visualize moments before/after the bat and ball make contact
+def visualize_swing(ax, frames, bat_buffer=0.07, ball_buffer=0.3):
+    """ Visualize moments before/after the bat and ball make contact (or closest approach).
     :param fig: MatplotLib figure to render on
     :param frames: PitchFrames w/ data for bat and ball
     :param bat_buffer: how many seconds before/after contact to display. default=0.07
     :param ball_buffer: how many seconds before/after contact to display. default=0.3
     :return:
     """
-    ax = fig.add_subplot(projection='3d')
 
     # calculate time of contact
-    t_contact = time_of_contact(frames.head, frames.handle, frames.ball)
+    t_contact = frames.hit_time
 
     # get frames near time of contact
-    ball_x, ball_y, ball_z, ball_time = extract_coords_from_frames(frames.ball, time_min=t_contact - ball_buffer,
-                                                                   time_max=t_contact + ball_buffer)
-    head_x, head_y, head_z, head_time = extract_coords_from_frames(frames.head, time_min=t_contact - bat_buffer,
-                                                                   time_max=t_contact + bat_buffer)
-    handle_x, handle_y, handle_z, _ = extract_coords_from_frames(frames.handle, time_min=t_contact - bat_buffer,
-                                                                 time_max=t_contact + bat_buffer)
+    ball_x, ball_y, ball_z, ball_time = extract_coords_from_frames(frames.ball, start=t_contact - ball_buffer,
+                                                                   end=t_contact + ball_buffer)
+    head_x, head_y, head_z, head_time = extract_coords_from_frames(frames.head, start=t_contact - bat_buffer,
+                                                                   end=t_contact + bat_buffer)
+    handle_x, handle_y, handle_z, _ = extract_coords_from_frames(frames.handle, start=t_contact - bat_buffer,
+                                                                 end=t_contact + bat_buffer)
 
-    # draw points
+    # draw ball
     ax.scatter(ball_x, ball_y, ball_z, c=ball_time, cmap=plt.colormaps['inferno'])
-    # ax.scatter(head_x, head_y, head_z, c=head_time, cmap=colormaps['Greens'])
-    # ax.scatter(handle_x, handle_z, handle_y, c=head_time, cmap=colormaps['Blues'])
 
-    # draw bat as a line
+    # draw bat
     cmap = plt.colormaps['inferno'].resampled(len(head_time))
     for i in range(len(head_time)):
         x = [head_x[i], handle_x[i]]
@@ -52,7 +53,7 @@ def visualize_pitch(fig, frames, bat_buffer=0.07, ball_buffer=0.3):
     plt.xlabel("x")
     plt.ylabel("y")
     set_axes_equal(ax)
-    fig.show()
+
 
 def set_axes_equal(ax):
     """
@@ -83,12 +84,14 @@ def set_axes_equal(ax):
 
 
 def time_of_contact(head_frames, handle_frames, ball_frames):
-    """ return time at which the ball and bat are closest to each other
+    """ return time at which the ball and bat are closest to each other.
+    Not actually required, since the bat tracking data has the time of hits - oops!
     :param head_frames: List[Frame]. tracking data for bat head
     :param handle_frames: List[Frame]. tracking data for bat handle.
     :param ball_frames: List[Frame]. tracking data for ball
     """
-    best_dist = 999  # closest distance between bat and ball
+    best_dist = np.Infinity  # closest distance between bat and ball
+    t_0 = None
     for ball in ball_frames:
         # find the index of head frame which is closest in time to the ball frame.
         # can use the index to get the position of both the head and handle, since they have the same time data
@@ -106,10 +109,12 @@ def time_of_contact(head_frames, handle_frames, ball_frames):
                       ball_pos - head_pos)  # area vector for 2x the triangle defined by the 3 points
         v2 = handle_pos - head_pos  # base of triangle
         dist = np.dot(v1, v1) / np.dot(v2, v2)
+
         if dist < best_dist:
             best_dist = dist
             t_0 = head.time
     return t_0
+
 
 def extract_coords_from_frames(frames: List[Frame], start=-999, end=999):
     """
@@ -122,35 +127,67 @@ def extract_coords_from_frames(frames: List[Frame], start=-999, end=999):
     time = [f.time for f in frames if start < f.time < end]
     return x, y, z, time
 
+
 @dataclass
 class PitchFrames:
     ball: List[Frame]
     head: List[Frame]
     handle: List[Frame]
+    hit_time: float
 
     def has_bat(self):
-        return self.head is not None or self.handle is not None
-
-    def has_ball(self):
-        return self.ball is not None
+        return self.head is not None and self.handle is not None
 
     @staticmethod
     def from_dict(samples_ball, samples_bat):
         """
-        Convert from format in JSONL files
+        Generate a PitchFrames object from the dictionaries in the JSONL files
         :param samples_ball: dict. samples_ball field in .jsonl
         :param samples_bat: dict. samples_bat field in .jsonl
         :return: PitchFrames object with frame data.
         """
 
+        # ball
         ball_frames = [Frame(s["time"], *s["pos"]) for s in samples_ball]
 
-        if samples_bat is None or samples_bat[0]["event"]=="No":
+        # bat
+        if samples_bat is None or samples_bat[0]["event"] == "No":
             head_frames = None
             handle_frames = None
         else:
+            # create Frame objects
             head_frames = [Frame(s["time"], *(s["head"]["pos"])) for s in samples_bat]
             handle_frames = [Frame(s["time"], *(s["handle"]["pos"])) for s in samples_bat]
 
-        return PitchFrames(ball_frames, head_frames, handle_frames)
+            # add velocity to Frames
+            PitchFrames._calculate_velocity(head_frames)
+            PitchFrames._calculate_velocity(handle_frames)
 
+        # get time of contact
+        temp = [s["time"] for s in samples_bat if ("event", "Hit") in s.items()]
+        try:
+            hit_time = temp[0]
+        except IndexError:
+            # indicates no hit event
+            hit_time = None
+
+        return PitchFrames(ball_frames, head_frames, handle_frames, hit_time)
+
+    @staticmethod
+    def _calculate_velocity(frames):
+        """
+        adds velocity information (speed and components) to a list of Frames
+        velocity is taken as average over a centred window of 10 frames.
+        does not update frames too close to list boundaries
+
+        updates frame object in place.
+        :param frames: list of Frame objects
+        :returns: None.
+        """
+        for start, mid, end in zip(frames[:-4], frames[2:-2], frames[4:]):
+            # mid takes average velocity over window [start, end]
+            delta_time = end.time - start.time
+            mid.vx = (end.x-start.x)/delta_time
+            mid.vy = (end.y-start.y)/delta_time
+            mid.vz = (end.z-start.z)/delta_time
+            mid.speed = np.sqrt(mid.vx**2 + mid.vy**2 + mid.vz**2)
